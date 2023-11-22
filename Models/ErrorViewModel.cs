46d5e59488ad364b6887847fdd62a2d4131de26a
pekinglc <liufly20000@hotmail.com>
2023-11-21 22:18:47 -0500

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace schoolEnrollment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
