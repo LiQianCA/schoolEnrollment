5907209b299c918d459e2889fa3973f8879092b8
LiQianCA <124838031+LiQianCA@users.noreply.github.com>
2023-11-21 23:07:48 -0500

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace schoolEnrollment.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
