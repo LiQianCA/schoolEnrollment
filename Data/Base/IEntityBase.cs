f46ee13b6a535384c56bb579bf650868ea464297
sun-qianfeng <124799740+sun-qianfeng@users.noreply.github.com>
2023-11-25 14:52:23 -0500

    update

--- Data/Base/IEntityBase.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LastFilm_Web_App.Data.Base;
-
-public interface IEntityBase
-{
-    int Id { get; set; }
-}
--- Data/Static/UserRoles.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LastFilm_Web_App.Data.Static;
-
-public static class UserRoles
-{
-    public const string Admin = "Admin";
-    public const string User = "User";
-}
